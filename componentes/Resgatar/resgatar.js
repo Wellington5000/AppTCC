e4dd3cf1c5c42a6bc894988873e54b7967109584
Wellington5000 <wellingtonsantos5000@gmail.com>
2022-01-09 18:03:05 -0300

    Tela resgatar

--- componentes/Resgatar/resgatar.js
+++ componentes/Resgatar/resgatar.js
@@ -15,6 +15,25 @@ const Resgatar = ({navigation}) => {
             Saldo Disponível                                                          58,00 R$
           </Text>
         </View>
+
+        <TouchableOpacity style={estilos.botaoResgatar}>
+            <Text style={estilos.textoResgatar}>Resgatar</Text>
+        </TouchableOpacity>
+
+        <View style={estilos.painelDadosBancarios}>
+            <Text style={estilos.textoDadosBancarios}>Dados Bancários</Text>
+            <Text style={estilos.textoDadosBancarios}>Conta                                                                                 64521522-9</Text>
+            <Text style={estilos.textoDadosBancarios}>Agência                                                                                     564213</Text>
+        </View>
+
+        <View style={estilos.painelPix}>
+            <Text style={estilos.textoDadosBancarios}>Pix</Text>
+            <Text style={estilos.textoDadosBancarios}>Chave                                                                         123.456.789-10</Text>
+        </View>
+
+        <TouchableOpacity style={estilos.botaoResgatar}>
+            <Text style={estilos.textoResgatar}>Alterar</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -27,6 +46,7 @@ const estilos = StyleSheet.create({
     backgroundColor: '#31C7D0',
   },
   painelBranco: {
+    alignItems: 'center',
     marginTop: 10,
     height: 670,
     width: 400,
@@ -55,6 +75,37 @@ const estilos = StyleSheet.create({
   textoTotalCashback: {
     color: 'white',
   },
+  botaoResgatar: {
+    width: 200,
+    height: 40,
+    backgroundColor: '#31C7D0',
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 30,
+    marginBottom: 100
+  },
+  textoResgatar: {
+      color: 'white',
+      fontSize: 18
+  },
+  painelDadosBancarios: {
+      width: 400,
+      height: 120,
+      backgroundColor: '#E9E9E9',
+      marginBottom: 30
+  },
+  painelPix: {
+      width: 400,
+      height: 80,
+      backgroundColor: '#E9E9E9',
+      marginBottom: 5
+  },
+  textoDadosBancarios: {
+      color: '#303539',
+      marginTop: 12,
+      marginLeft: 10
+  }
 });
 
 export default Resgatar;
